463719ddc80c39f997210c060167f03e791f7198
iveleckis <ignas.veleckis@outlook.com>
2026-09-02 22:45:35 +0300

    chore: api layer

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./providers/auth/AuthProvider";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30_000,
+      retry: 1,
+    },
+  },
+});
+
 function App() {
-  return <AuthProvider>app</AuthProvider>;
+  return;
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>app</AuthProvider>;
+  </QueryClientProvider>;
 }
 
 export default App;
